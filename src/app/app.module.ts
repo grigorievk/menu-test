fa00895e95414e00b3aeb60e342a4f2b376942fa
Konstantin <konstantin@love2shop.ru>
2019-01-28 11:29:07 +0300

    Basically page components and layouts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { PHomeComponent } from './pages/p-home/p-home.component';
+import { PDashboardComponent } from './pages/p-dashboard/p-dashboard.component';
+import { PNotfoundComponent } from './pages/p-notfound/p-notfound.component';
+
+const AppRoutes: Routes = [
+  {path: '', component: PHomeComponent},
+  {path: 'dashboard', component: PDashboardComponent},
+  {path: '**', component: PNotfoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PHomeComponent,
+    PDashboardComponent,
+    PNotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(AppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
