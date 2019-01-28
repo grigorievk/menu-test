bcc1ddef80f7d77fe44982682fa95cd65f8759c8
Konstantin <konstantin@love2shop.ru>
2019-01-28 21:57:13 +0300

    Add menu active mark

--- src/app/blocks/b-aside-menu/b-aside-menu.component.ts
+++ src/app/blocks/b-aside-menu/b-aside-menu.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class BAsideMenuComponent implements OnInit {
   @Input() menuItems;
+  @Input() categoryId;
 
   constructor() { }
 
