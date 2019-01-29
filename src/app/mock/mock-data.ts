2dd78254f02d0979a09822c3581e71ebea85886e
Konstantin <konstantin@love2shop.ru>
2019-01-29 22:57:58 +0300

    Mock data

---
+++ src/app/mock/mock-data.ts
@@ -0,0 +1,37 @@
+export const MOCKMENU = [
+    {
+        id: 1,
+        name : 'Menu 1'
+    },
+    {
+        id: 2,
+        name : 'Menu 2'
+    }
+];
+
+export const MOCKITEMS: object = {
+    1: {
+        items: [
+            {id: 1, name: 'Item 1', price: 50},
+            {id: 2, name: 'Item 2', price: 70},
+            {id: 3, name: 'Item 3', price: 60},
+            {id: 4, name: 'Item 4', price: 5},
+            {id: 5, name: 'Item 5', price: 7},
+            {id: 6, name: 'Item 6', price: 17},
+            {id: 7, name: 'Item 7', price: 23},
+        ]
+    },
+    2: {
+        items: [
+            {id: 8, name: 'Item 1', price: 71},
+            {id: 9, name: 'Item 2', price: 55},
+            {id: 10, name: 'Item 3', price: 6},
+            {id: 11, name: 'Item 4', price: 52},
+            {id: 12, name: 'Item 5', price: 18},
+            {id: 13, name: 'Item 6', price: 27},
+            {id: 14, name: 'Item 7', price: 33},
+        ]
+    }
+};
+
+
--- src/app/pages/p-dashboard/p-dashboard.component.ts
+++ src/app/pages/p-dashboard/p-dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { CountService } from '../../count.service';
+import { MOCKMENU, MOCKITEMS } from '../../mock/mock-data';
 
 @Component({
   selector: 'app-p-dashboard',
@@ -8,69 +9,37 @@ import { CountService } from '../../count.service';
   styleUrls: ['./p-dashboard.component.sass']
 })
 export class PDashboardComponent implements OnInit {
-    menuArray = [
-        {
-            id: 1,
-            name : 'Menu 1'
-        },
-        {
-            id: 2,
-            name : 'Menu 2'
-        }
-    ];
-    mockItems = {
-      1: {
-          items: [
-              {id: 1, name: 'Item 1', category: 1, price: 50},
-              {id: 2, name: 'Item 2', category: 1, price: 70},
-              {id: 3, name: 'Item 3', category: 1, price: 60},
-              {id: 4, name: 'Item 4', category: 1, price: 5},
-              {id: 5, name: 'Item 5', category: 1, price: 7},
-              {id: 6, name: 'Item 6', category: 1, price: 17},
-              {id: 7, name: 'Item 7', category: 1, price: 23},
-          ]
-      },
-      2: {
-          items: [
-              {id: 8, name: 'Item 1', category: 2, price: 71},
-              {id: 9, name: 'Item 2', category: 2, price: 55},
-              {id: 10, name: 'Item 3', category: 2, price: 6},
-              {id: 11, name: 'Item 4', category: 2, price: 52},
-              {id: 12, name: 'Item 5', category: 2, price: 18},
-              {id: 13, name: 'Item 6', category: 2, price: 27},
-              {id: 14, name: 'Item 7', category: 2, price: 33},
-          ]
-      }
-    };
+    mockMenu = MOCKMENU;
+    mockItems = MOCKITEMS;
 
     menuTitle: string;
     listItems: object;
     categoryId: number;
 
-  constructor(
-      private router: Router,
-      private route: ActivatedRoute,
-      private countService: CountService
-  ) {}
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute,
+        private countService: CountService
+    ) {}
 
-  ngOnInit() {
-     this.route.paramMap.subscribe(params => {
-        this.categoryId = parseInt(params.get('id')) || 1;
-        this.menuTitle = this.menuArray.filter((obj) => {
-            return obj.id === this.categoryId;
-        })[0].name;
+    ngOnInit() {
+        this.route.paramMap.subscribe(params => {
+            this.categoryId = parseInt(params.get('id')) || 1;
+            this.menuTitle = this.mockMenu.filter((obj) => {
+                return obj.id === this.categoryId;
+            })[0].name;
 
-        this.getItems()
-            .then((items) => {
-                this.listItems = this.prepare(items);
-            })
-            .catch(() => {
-                this.router.navigate(['/404']);
-            });
-     });
-  }
+            this.getItems()
+                .then((items) => {
+                    this.listItems = this.prepare(items);
+                })
+                .catch(() => {
+                    this.router.navigate(['/404']);
+                });
+        });
+    }
 
-  getItems() {
+    getItems() {
       return new Promise((resolve, reject) => {
           if (this.mockItems[this.categoryId]) {
               resolve(this.mockItems[this.categoryId].items);
@@ -79,14 +48,14 @@ export class PDashboardComponent implements OnInit {
 
           reject();
       });
-  }
+    }
 
-  prepare(items) {
-    return items.map((item) => {
-        item.checked = this.countService.selectedItems.find(item2 => item2.id === item.id);
+    prepare(items) {
+        return items.map((item) => {
+            item.checked = this.countService.selectedItems.find(item2 => item2.id === item.id);
 
-        return item;
-    });
-  }
+            return item;
+        });
+    }
 
 }
